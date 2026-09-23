52bbdfb846c2fcb87b3406e2a03a584d2adf6155
Ksawier Wilczynski <57848442+KsawierWilczynski@users.noreply.github.com>
2026-09-23 09:58:04 +0100

    [Lab] Delete a task

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -18,6 +18,11 @@ function App() {
     console.log(`${taskIndex} ${tasks[taskIndex].done}`);
   }
 
+  const deleteHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks.splice(taskIndex, 1);
+    setTaskState({tasks});
+  } 
 
   return (
     <div className="container">
@@ -31,6 +36,7 @@ function App() {
           priority={task.priority}
           done={task.done}
           markDone={() => doneHandler(index)}
+          deleteTask={() => deleteHandler(index)}
         />
       ))}
     </div>
