8ef8aec37666f22c5dc1e422223e72ebfdd5b893
Ksawier Wilczynski <57848442+KsawierWilczynski@users.noreply.github.com>
2026-09-23 10:38:09 +0100

    [Lab] Lab03 complete

--- tasky/src/components/Form.jsx
+++ tasky/src/components/Form.jsx
@@ -1,33 +1,66 @@
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
 const AddTaskForm = (props) => {
 
   return (
+   <Box
+    component="form"
+    sx={{
+        '& .MuiOutlinedInput-root': { m: 1, width: '30ch' },
+    }}
+    onSubmit={props.submit}
+    >
     <div>
-      <form onSubmit={props.submit}>
-        <label>
-            Task title:
-            <input type="text" name="title" required onChange={(event) => props.change(event)} />
-        </label>
-        <br />
-        <label>
-            Due date:
-            <input type="date" name="deadline" required onChange={(event) => props.change(event)} />
-        </label>
-        <br />
-        <label>
-            Details:
-            <input type="text" name="description" onChange={(event) => props.change(event)} />
-        </label>
-        <label>
-            Priority:
-            <select name="priority" required onChange={(event) => props.change(event)}>
-                <option value="low" selected>Low</option>
-                <option value="medium">Medium</option>
-                <option value="high">High</option>
-            </select>
-        </label>
-        <input type="submit" value="Submit" />
-        </form>
+        <TextField
+        required
+        id="outlined-required"
+        name="title"
+        label="Task Title"
+        slotProps={{ inputLabel: { shrink: true } }}
+        onChange={(event) => props.change(event)}
+        />
     </div>
+
+    <div>
+        <TextField
+        required
+        name="deadline"
+        label="Deadline"
+        slotProps={{ inputLabel: { shrink: true } }}
+        type="date"
+        onChange={(event) => props.change(event)}
+        />
+    </div>
+
+    <div>
+        <TextField
+        name="description"
+        id="outlined-multiline-static"
+        label="Task Details"
+        slotProps={{ inputLabel: { shrink: true } }}
+        multiline
+        rows={4}
+        onChange={(event) => props.change(event)}
+        />
+    </div>
+
+    <div>
+        <Button
+        type="submit"
+        variant="contained"
+        color="primary"
+        sx={{
+            m: 1,
+            p: 1,
+            width: '95%'
+        }}
+        >
+        Add Task
+        </Button>
+    </div>
+    </Box>
   )
 };
 
